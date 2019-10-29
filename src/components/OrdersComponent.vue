077158f510c59067c33ceb4e8e5f7a98c2485f09
baiceng <jameslu8044@gmail.com>
2019-10-29 10:06:29 +0800

    order vuetify update

--- src/components/OrdersComponent.vue
+++ src/components/OrdersComponent.vue
@@ -1,151 +1,125 @@
 <template>
-  <div>
-    <v-layout align-center overflow-auto>
-      <v-flex xs12>
-        <v-data-table
-          v-model="selected"
-          :headers="headers"
-          :items="orders"
-          :search="search"
-          :pagination.sync="pagination"
-          :loading="loading"
-          :rows-per-page-items="rowsPerPage"
-          select-all
-          class="elevation-1"
-        >
-          <template v-slot:items="props">
-            <td>
-              <v-checkbox
-                v-model="props.selected"
-                primary
-                hide-details
-              ></v-checkbox>
-            </td>
-            <td>{{ props.item.o_no }}</td>
-            <td class="text-xs-right">{{ props.item.o_date }}</td>
-            <td class="text-xs-center">{{ props.item.o_seller_name }}</td>
-            <td class="text-xs-center">{{ props.item.o_buyer_name }}</td>
-            <td class="text-xs-center">{{ props.item.o_product_name }}</td>
-            <td class="text-xs-right">{{ props.item.o_product_part_no }}</td>
-            <td class="text-xs-center">{{ props.item.o_product_spec }}</td>
-            <td class="text-xs-right">{{ Math.round(props.item.o_product_price * 1000) / 1000 }}</td>
-            <td class="text-xs-center">{{ props.item.o_currency }}</td>
-            <td class="text-xs-right">{{ Math.round(props.item.o_quantity * 1000) / 1000 }}</td>
-            <td class="text-xs-right">{{ props.item.o_amount }}</td>
-            <td class="text-xs-right">{{ props.item.o_note }}</td>
-            <td id="actions" class="justify-center layout px-0">
-              <v-btn icon small class="ma-0 editBtn">
-                <v-icon
-                  small
-                  @click="editedItem(props.item)"
-                >
-                  edit
-                </v-icon>
-              </v-btn>
-              <v-btn icon small class="ma-0 deleteBtn">
-                <v-icon
-                  small
-                  @click="deleteItem(props.item)"
-                >
-                  delete
-                </v-icon>
-              </v-btn>
-            </td>
-          </template>
-          <template v-slot:no-data>
-            <v-alert :value="noDataAlert" color="error" icon="warning">
-              抱歉，這裡沒有任何資料 :(
-            </v-alert>
-          </template>
-        </v-data-table>
-      </v-flex>
-    </v-layout>
+  <v-data-table
+    v-model="selected"
+    :headers="headers"
+    :items="orders"
+    :search="search"
+    :options.sync="options"
+    :footer-props="footerProps"
+    :server-items-length="totalItems"
+    :loading="loading"
+    show-select
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <!--  Dialog  -->
+      <v-dialog
+        v-model="dialog"
+        max-width="800px"
+        persistent
+      >
+        <v-card>
+          <v-card-title>
+            <span class="headline">
+              {{ formTitle }}
+            </span>
+          </v-card-title>
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-card-text>
+              <v-container grid-list-md>
+                <v-row wrap>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field :rules="[rules.required]" v-model="editItem.o_no" label="訂單號碼" maxlength="30"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-menu
+                      v-model="menu"
+                      :close-on-content-click="false"
+                      :nudge-right="40"
+                      lazy
+                      transition="scale-transition"
+                      offset-y
+                      full-width
+                      min-width="290px"
+                    >
+                      <template v-slot:activator="{ on }">
+                        <v-text-field
+                          v-model="editItem.o_date"
+                          prepend-icon="event"
+                          label="訂單日期"
+                          :rules="[rules.required]"
+                          readonly
+                          v-on="on"
+                        ></v-text-field>
+                      </template>
+                      <v-date-picker v-model="editItem.o_date" @input="menu = false"></v-date-picker>
+                    </v-menu>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field :rules="[rules.required]" v-model="editItem.o_seller_name" label="賣家名稱"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field :rules="[rules.required]" v-model="editItem.o_buyer_name" label="買家名稱"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field :rules="[rules.required]" v-model="editItem.o_product_name" label="產品名稱"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field :rules="[rules.required]" v-model="editItem.o_product_part_no" label="產品料號" maxlength="30"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editItem.o_product_spec" label="產品規格"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field type="number" v-model="editItem.o_product_price" label="產品價格" :rules="[rules.required]" maxlength="12"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field :rules="[rules.required]" v-model="editItem.o_currency" label="幣別" maxlength="10"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field :rules="[rules.required]" v-model="editItem.o_quantity" label="採購數量"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="amount" label="採購金額"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editItem.o_note" label="付款條件"></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="save" :disabled="!valid">Save</v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card>
+      </v-dialog>
+    </template>
 
-    <!--  Dialog  -->
-    <v-dialog
-      v-model="dialog"
-      max-width="800px"
-      persistent
-    >
-      <v-card>
-        <v-card-title>
-          <span class="headline">
-            {{ formTitle }}
-          </span>
-        </v-card-title>
-        <v-form ref="form" v-model="valid" lazy-validation>
-          <v-card-text>
-            <v-container grid-list-md>
-              <v-layout wrap>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field :rules="[rules.required]" v-model="editItem.o_no" label="訂單號碼" maxlength="30"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-menu
-                    v-model="menu"
-                    :close-on-content-click="false"
-                    :nudge-right="40"
-                    lazy
-                    transition="scale-transition"
-                    offset-y
-                    full-width
-                    min-width="290px"
-                  >
-                    <template v-slot:activator="{ on }">
-                      <v-text-field
-                        v-model="editItem.o_date"
-                        prepend-icon="event"
-                        label="訂單日期"
-                        :rules="[rules.required]"
-                        readonly
-                        v-on="on"
-                      ></v-text-field>
-                    </template>
-                    <v-date-picker v-model="editItem.o_date" @input="menu = false"></v-date-picker>
-                  </v-menu>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field :rules="[rules.required]" v-model="editItem.o_seller_name" label="賣家名稱"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field :rules="[rules.required]" v-model="editItem.o_buyer_name" label="買家名稱"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field :rules="[rules.required]" v-model="editItem.o_product_name" label="產品名稱"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field :rules="[rules.required]" v-model="editItem.o_product_part_no" label="產品料號" maxlength="30"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field v-model="editItem.o_product_spec" label="產品規格"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field type="number" v-model="editItem.o_product_price" label="產品價格" :rules="[rules.required]" maxlength="12"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field :rules="[rules.required]" v-model="editItem.o_currency" label="幣別" maxlength="10"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field :rules="[rules.required]" v-model="editItem.o_quantity" label="採購數量"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field v-model="amount" label="採購金額"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field v-model="editItem.o_note" label="付款條件"></v-text-field>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
-            <v-btn color="blue darken-1" flat @click="save" :disabled="!valid">Save</v-btn>
-          </v-card-actions>
-        </v-form>
-      </v-card>
-    </v-dialog>
-  </div>
+    <template v-slot:item.action="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="editedItem(item)"
+      >
+        edit
+      </v-icon>
+      <v-icon
+        small
+        @click="deleteItem(item)"
+      >
+        delete
+      </v-icon>
+    </template>
+
+    <template v-slot:no-data>
+      <v-alert :value="noDataAlert" color="error" icon="warning">
+        抱歉，這裡沒有任何資料 :(
+      </v-alert>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
@@ -155,10 +129,13 @@ export default {
     return {
       value: 123,
       valid: true,
-      rowsPerPage: [10,25,50,{"text":"$vuetify.dataIterator.rowsPerPageAll","value":-1}],
       menu: false,
       noDataAlert: false,
-      pagination: { rowsPerPage: 25, sortBy: 'id' },
+      options: {},
+      totalItems: 0,
+      footerProps: {
+        'items-per-page-options': [10, 25, 50, 100, 200, 500]
+      },
       orders: [],
       loading: true,
       headers: [
@@ -235,19 +212,40 @@ export default {
       this.$emit('update:selected', this.selected);
       this.$emit('getDataType', 'orders');
     },
+    options: {
+      handler() {
+        this.loading = true
+        this.getDataFromApi()
+          .then(data => {
+            this.orders = data.data
+            this.totalItems = data.total
+            this.loading = false
+          }) 
+      },
+      deep: true
+    }
   },
   created() {
-    this.getOrders()
+  },
+  mounted() {
+    this.loading = true
+    this.getDataFromApi()
+      .then(data => {
+        this.orders = data.data
+        this.totalItems = data.total
+        this.loading = false
+      })
   },
   methods: {
-    getOrders () {
-      this.axios.get('orders').then(response => {
-        this.orders = response.data;
-        this.noDataAlert = true;
-        this.loading = false;
-      }).catch(error => {
-        this.flash(error.message, 'error');
-      });
+    getDataFromApi() {
+      return new Promise((resolve, reject) => {
+        const { page, itemsPerPage } = this.options
+        const url = `orders?page=${page}&itemsPerPage=${itemsPerPage}`
+        this.axios.get(url)
+          .then(res => {
+            return resolve(res.data)
+          })
+      })
     },
     editedItem (item) {
       this.editIndex = this.orders.indexOf(item);
@@ -265,12 +263,11 @@ export default {
     },
     close () {
       this.$emit('toggleDialog', false);
-      const root = this
-      setTimeout(function () {
-        root.$refs.form.reset();
+      setTimeout(() => {
+        this.$refs.form.reset();
         this.editItem = Object.assign({}, this.defaultItem);
         this.editIndex = -1;
-      },3000)
+      },1000)
     },
     save () {
       let index = this.editIndex;
